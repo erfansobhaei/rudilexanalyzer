17df2eed8ef5c67ab496199f502d7d398c93414b
Erfan Sobhaei <e.sobhaei@gmail.com>
2021-01-03 00:09:41 +0330

    Lexical tokens are found

--- rudilexanalyzer.py
+++ rudilexanalyzer.py
@@ -1,4 +1,20 @@
 import re
 
-terminal_pattern = re.compile(r"~([^%~ ]*)")
-nonterminal_pattern = re.compile(r"%([\w+][?\w]*)")
+# Specifying grammar patterns
+pattern_list = [ r"~([^%~ ]*)", r"%([\w+][?\w]*)"]
+terminal_regex = re.compile(pattern_list[0])
+nonterminal_regex = re.compile(pattern_list[1])
+token_regex = re.compile(r"{}|{}".format(pattern_list[0],pattern_list[1]))
+
+# Opening grammar file
+with open("grammar_comment.txt", 'r') as reader:
+    # Reading all lines
+    lines = reader.readlines()
+
+    # Removing blank lines
+    while '\n' in lines: lines.remove('\n')
+
+    # Finding all terminals and nonterminals
+    tokens = token_regex.findall(s)
+
+    
